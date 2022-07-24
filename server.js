61d2cf2051a3e111097c48b688425d87269b7d70
Parker McKillop <mckilpar000@hotmail.com>
2022-07-24 15:14:49 -0700

    basic server completed, listening and serving index.html

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
-const db = require("./db.json");
+const db = require("./db/db.json");
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./index.html"));
+  });
+
+app.listen(PORT, () =>{
+    console.log(`Listening for note requests on ${PORT}`)
+})
